5d9fa4f73c105704efbcef7955f670db478c8d79
martinyounghoonkim <hoons0131@gmail.com>
2017-11-25 13:41:17 +0900

    Set bootstrap packages in entry file.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+/**
+ * @desc use BootstrapVue packages
+ */
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 new Vue({
   el: '#app',
